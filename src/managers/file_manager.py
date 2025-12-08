5008d23cce79aa4dab52aa379c22e30435c3b6d4
siyounglee-korea <syoung8296@gmail.com>
2025-12-08 23:41:12 +0900

    Revert "create_framework_251208"
    This reverts commit 378d8ffbb9c1e021825b8096d50e9f6bae6b1397.

--- src/managers/file_manager.py
+++
@@ -1,7 +0,0 @@
-from utils.headers import *
-
-## csv, json read / save 등 파일을 관리 하는 곳입니다. 
-
-class FileManager:
-    def save_json_file(self, file_name, file_data, option="w"):
-        pass
--- src/pages/main_page.py
+++
@@ -1,9 +0,0 @@
-from utils.headers import *
-
-from pages.base_page import BasePage
-from utils.defines import TARGET_URL
-
-class MainPage(BasePage):
-    def go_to_main_page(self):
-        self.go_to_page(TARGET_URL["MAIN_URL"])
-        time.sleep(1)
