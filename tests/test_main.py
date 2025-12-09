65883c81d3c9bb455c84e1d696077c74b924afc8
sujin7373 <sujinsujin7733@gmail.com>
2025-12-09 11:03:36 +0900

    개인 브랜치 확인 테스트

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,9 @@
 from utils.headers import *
 
 
-def test_main(main_page):
+def test_main1(main_page):
+    main_page.go_to_main_page()
+    
+def test_main2(main_page):
     main_page.go_to_main_page()
     
