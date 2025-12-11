ea1a9e61f7cdca9527101ffae801a53e802d3cd1
siyounglee-korea <syoung8296@gmail.com>
2025-12-11 13:07:41 +0900

    QHCQ-5 사파리, element 함수 추가
    driver_managers-safari 추가
    base_page-get element 함수 추가
    login_page-input id, pw 함수 추가
    defines-client 사이즈 받아오는 함수 추가
    headers -safari import 추가

--- src/utils/defines.py
+++ src/utils/defines.py
@@ -1,14 +1,28 @@
+import pyautogui
 # define를 모으는 곳입니다.
 # 각자 사용해야하는 selector나 xpath를 작성해주세요!
 
 # 예시입니다!!
+SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+FULLSCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+
 TARGET_URL = {
     "MAIN_URL": "https://qaproject.elice.io/ai-helpy-chat",
 }
 
 
 SELECTORS = {
-    "BTN_LOGIN": "//a[@href='/login']",    
-    
+
+  
 }
 
+# By.NAME
+NAME = {
+    "INPUT_ID" : "loginId",    
+    "INPUT_PW" : "password",    
+}
+
+XPATH = {
+    "BTN_LOGIN": "//button[normalize-space()='Login']",
+}
