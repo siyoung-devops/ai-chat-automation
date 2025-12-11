25f0c7d91ad5b465ee074beddc20c7d96c873dd3
sujin7373 <sujinsujin7733@gmail.com>
2025-12-11 19:20:42 +0900

    QHCQ-5 singup success test complete

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -13,8 +13,15 @@ def test_signup_success() :
         page = SignupPage(driver)
         
         page.go_to_signup_page()
-        page.signup_email("hihello@gmail.com")
+        page.signup_email("bimisi@gmail.com")
         page.signup_pw("asdf1234!")
+        page.signup_name("이수진")
+        elements = page.signup_checkbox()
+        elements[0].click()
+        page.btn_create()
+        success = page.check_signup_success()
+        
+        assert "Forgot" in success, "PHC-TS02-TC001 : Test fail"
         
     finally :
         driver.quit()
